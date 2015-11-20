cf444369a07f5d6c3567045ade7e0aced4fdaad4
momozqy <532676071@qq.com>
2015-11-20 11:26:44 +0800

    我的定制
    更新布局。

--- src/com/jay/example/fragmentforhost/Fragment3.java
+++ src/com/jay/example/fragmentforhost/Fragment3.java
@@ -5,12 +5,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Fragment3 extends Fragment {
+	ListView list;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fg3, container,false);
+		list = (ListView) view.findViewById(R.id.mineOrder);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1, new String[]{"我的定制","历史记录"});
+		list.setAdapter(adapter);
 		return view;
 	}
 }
